0cc1756b30f763fcd784096e11f7cf6d35eabc3e
pgk <pgk977@outlook.com>
2021-04-29 11:00:13 +0200

    kepesito

---
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/phonebook/Phonebook.java
@@ -0,0 +1,44 @@
+package hu.nive.ujratervezes.kepesitovizsga.phonebook;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class Phonebook {
+
+
+    public void exportPhonebook(Map<String, String> contacts, String output){
+
+        if(contacts==null || output == null){
+            throw new IllegalArgumentException();
+        }
+
+        try (BufferedWriter writer = Files.newBufferedWriter(Path.of(output))) {
+
+            for (Map.Entry<String, String> entry: contacts.entrySet()) {
+
+                if(contacts.size()>1) {
+                    String line = entry.getKey() + ": " + entry.getValue() + ", ";
+                }
+
+                else {
+                    writer.write(entry.getKey() + ": " + entry.getValue());
+                }
+
+            }
+
+
+
+
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not write file", ioe);
+        }
+
+
+    }
+
+
+}
