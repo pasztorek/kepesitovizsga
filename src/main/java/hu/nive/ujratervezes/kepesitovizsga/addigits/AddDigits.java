d991973d23acea06f2244556a71b0eb80622f288
pgk <pgk977@outlook.com>
2021-04-29 10:30:22 +0200

    kepesito

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
@@ -2,5 +2,25 @@ package hu.nive.ujratervezes.kepesitovizsga.addigits;
 
 public class AddDigits {
 
+public int  addDigits(String input){
+    int sum=0;
+
+    if(input == null || input.equals("")){
+        return -1;
+    }
+
+    for(int i=0; i<input.length(); i++){
+        if (input.charAt(i)>=49 && input.charAt(i)<=57){
+                int charnum = Integer.parseInt(input.substring(i,i+1));
+
+                sum = sum + charnum;
+        }
+
+    }
+
+    return sum;
+}
+
+
 
 }
